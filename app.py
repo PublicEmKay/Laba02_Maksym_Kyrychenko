68d3486469c5363d213e537e1eb1438e5dd2b6b1
Maxim Kirichenko <public.emkay@gmail.com>
2023-09-21 13:09:09 +0300

    Creation of the application code

---
+++ app.py
@@ -0,0 +1,12 @@
+import json
+import random
+
+def lambda_handler(event, context):
+    # Генеруємо випадкове число від 1 до 6
+    random_number = random.randint(1, 6)
+    
+    # Повертаємо результат у вигляді об'єкта JSON
+    return {
+        'statusCode': 200,
+        'body': f'<p>Випадкове число: {random_number}</p>'
+    }
